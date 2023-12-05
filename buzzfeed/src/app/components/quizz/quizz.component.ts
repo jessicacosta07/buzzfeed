f8f77431c5c82cfc2d6c57a11d6c0928b6b97881
jessicacosta07 <jessikacostajf@gmail.com>
2023-12-05 17:36:20 -0300

    criando lógica

--- buzzfeed/src/app/components/quizz/quizz.component.ts
+++ buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { type } from 'os';
 
 @Component({
   selector: 'app-quizz',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+  
+export class QuizzComponent implements OnInit{
+    
+  title: string = ""
+  questions: any
+  questionSelected: any
 
+  answers: string[] = []
+  answerSelected: string = ""
+  
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
 }
